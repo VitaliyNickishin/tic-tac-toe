a360fc4edf8acc988dc14040741df22c7ee0695e
“VitaliyNickishin” <nickishin34@gmail.com>
2019-04-29 18:53:41 +0300

    makig an interactive component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+ constructor(props) {
+  super(props);
+  this.state = {
+   value: null,
+  };
+ }
  render() {
    return (
-     <button className="square">
-       {/* TODO */}
+     <button 
+      className="square" 
+      onClick={() => this.setState({value: 'X'})}
+     >
+      {this.state.value}
      </button>
    );
  }
@@ -14,7 +23,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
  renderSquare(i) {
-   return <Square />;
+   return <Square value = {i} />;
  }
 
  render() {
@@ -58,6 +67,23 @@ class Game extends React.Component {
    );
  }
 }
+
+/*
+class ShoppingList extends React.Component {
+ render() {
+  return (
+   <div className="shopping-list">
+    <h1>Shopping List for {this.props.name}</h1>
+    <ul>
+     <li>Instagram</li>
+     <li>WhatsApp</li>
+     <li>Oculus</li>
+    </ul>
+   </div>
+  );
+ }
+}
+*/
 
 // ========================================
 
